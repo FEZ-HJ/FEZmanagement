5c99ae238b92e7a6584ccf0f86059f90dde1d709
FEZ-HJ <fez-hj@163.com>
2018-10-01 18:28:28 +0800

    Initial commit

---
+++ FEZ-security-demo/src/main/java/com/fez/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.fez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by H.J
+ * 2018/10/1
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security";
+    }
+}

368deac7c213869476b22d08803a11cfa816bb89
FEZ-HJ <fez-hj@163.com>
2018-10-06 23:34:49 +0800

    多线程

---
+++ FEZ-security-demo/src/main/java/com/fez/web/async/DeferredResultHolder.java
@@ -0,0 +1,19 @@
+package com.fez.web.async;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by H.J
+ * 2018/10/6
+ */
+@Component
+@Data
+public class DeferredResultHolder {
+
+    private Map<String,DeferredResult<String>> map = new HashMap<>();
+}
